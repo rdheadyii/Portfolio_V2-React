8761c2834accda892e1d6117f79b219735c064fc
rdheadyii <rdheadyii@gmail.com>
2023-09-04 14:53:01 -0400

    wrote header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import NavBar from './NavBar';
+
+export default function Header() {
+
+    return (
+        <header>
+            <h1>
+                Russell D Heady II
+            </h1>
+
+            <NavBar />
+        </header>
+    );
+}
