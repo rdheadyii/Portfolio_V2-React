42ef7e4f7db2189d8ccaa9f4f7728a269ea08226
rdheadyii <rdheadyii@gmail.com>
2023-09-04 15:39:27 -0400

    wrote resume component

---
+++ src/pages/Resume.jsx
@@ -0,0 +1,63 @@
+export default function Resume() {
+	return (
+		<div>
+			<h2>Resume</h2>
+            <a href='' download=''>
+			</a>
+			<div>
+				<h3>Education</h3>
+				<ul>
+					<li>University of Central Florida Coding BootCamp
+						<ul>
+							<li>Full Stack Web Development</li>
+						</ul>
+					</li>
+
+					<li> Florida Southern College
+						<ul>
+							<li>Bachelor’s Degree in Exercise Science</li>
+						</ul>
+					</li>
+
+                    <li> Polk State College
+						<ul>
+							<li>Associate’s Degree in </li>
+						</ul>
+					</li>
+				</ul>
+
+				<h3>Technical skills</h3>
+				<ul>
+					<li>Full Stack Web Development
+						<ul>
+							<li>Front-End Proficiencies
+                                <ul>
+                                    <li>HTML</li>
+                                    <li>CSS</li>
+                                    <li>JavaScript</li>
+                                    <li>jQuery</li>
+                                    <li>Responsive Design</li>
+                                    <li>React</li>
+                                    <li>Bootstrap</li>
+                                    <li>Tailwind CSS</li>
+                                </ul>
+                            </li>
+                            
+                            <li>Back-End Proficiencies
+                                <ul>
+                                    <li>APIs</li>
+                                    <li>Node.js</li>
+                                    <li>Express</li>
+                                    <li>MySQL, Sequelize</li>
+                                    <li>MongoDB, Mongoose</li>
+                                    <li>REST</li>
+                                    <li>GraphQL</li>
+                                </ul>
+                            </li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
+}
