0f45a0cd9a8ce2975fa4a9ae90832137b7c06ef5
rdheadyii <rdheadyii@gmail.com>
2023-09-04 14:46:28 -0400

    wrote about me section

---
+++ src/pages/About.jsx
@@ -0,0 +1,16 @@
+export default function About() {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>
+                Hello! My name is Russell, and I am 26 years old. I am a compelled individual consistently engaging in fresh challenges. I have a strong work ethic as well as being very motivated. I consider myself a fast learner who engages to ensure skills needed are superior; nothing less than my all. I am a team player not resentful to opinions of others. I handle on the job constructive criticism to an advantage to better myself and the task at hand. I keep a positive attitude in all areas of life.
+            </p>
+
+            <br />
+
+            <p>
+            I have a Bachelor's degree which I used for 4 years and have decided to make a career change to pursue my next endeavors in the field of software development. Computers have always been my passion, when working with computers I do not feel it is a job but more of a hobby. When engaging in software development, I gain a sense of euphoria. Software development is a rapidly growing field, with a plethora of opportunities within multiple realms of businesses. I am confident that I have the abilities and commitment to be a positive asset in the software development field . I look forward to the opportunities the future holds for me.
+            </p>
+        </div>
+    );
+}
