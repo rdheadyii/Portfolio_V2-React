d513dd210c6054ab722ac045a754fc14aa3037a2
rdheadyii <rdheadyii@gmail.com>
2023-09-04 11:14:39 -0400

    fixed spelling of portfolio page/wrote main jsx for routes/cleared index css of default css

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,49 @@
+// import in necessary items to get router functionality 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css'
 
+// import in all pages
+import App from './App.jsx';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+
+// defining routes that are available and components generated at each route
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/about',
+        element: <About />
+      },
+      {
+        path: '/portfolio',
+        element: <Portfolio />
+      },
+      {
+        path: '/contact',
+        element: <Contact />
+      },
+      {
+        path: '/resume',
+        element: <Resume />
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
