8a9f58ad38e828d66a85298354458f3da7663043
rdheadyii <rdheadyii@gmail.com>
2023-09-04 14:59:59 -0400

    wrote navbar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,37 @@
+import { Link, useLocation } from 'react-router-dom';
+
+function NavBar() {
+    const currentPage = useLocation().pathname;
+
+    return (
+        <ul>
+            <li>
+                <Link to="/">
+                Home
+                </Link>
+            </li>
+            <li>
+                <Link to="/about">
+                About
+                </Link>
+            </li>
+            <li>
+                <Link to="/portfolio">
+                Portfolio
+                </Link>
+            </li>
+            <li>
+                <Link to="/contact">
+                Contact
+                </Link>
+            </li>
+            <li>
+                <Link to="/resume">
+                Resume
+                </Link>
+            </li>
+        </ul>
+    );
+}
+
+export default NavBar;
